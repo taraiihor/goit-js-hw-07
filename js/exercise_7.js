c411372e61477c70b96aad26afa130691183b93b
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-09-22 21:45:50 +0300

    Update exercise_7.js

--- js/exercise_7.js
+++ js/exercise_7.js
@@ -1,22 +1,9 @@
-// const inputEl = document.querySelector('#font-size-control');
-// const textEl = document.querySelector('#text');
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
 
-// inputEl.addEventListener('input', onTextSize);
+inputEl.addEventListener('input', onTextSize);
 
-// function onTextSize(event) {
-//   textEl.style.fontSize = event.currentTarget.value;
-//   console.log(textEl.style.fontSize);
-// }
-const refs = {
-  input: document.querySelector('#font-size-control'),
-  text: document.querySelector('#text'),
-};
-
-refs.input.addEventListener('input', onInputFontSizeControl);
-
-function onInputFontSizeControl(event) {
-  // console.log(event.currentTarget.value + 'px');
-
-  refs.text.style.fontSize = event.currentTarget.value + 'px';
-  console.log(refs.text.style.fontSize);
+function onTextSize(event) {
+  textEl.style.fontSize = event.currentTarget.value;
+  console.log(textEl.style.fontSize);
 }
